32cb27c8a89918ea99971bd300b9f7e412fca821
AntoninDev <118391001+AntoninDev@users.noreply.github.com>
2022-12-09 22:25:10 -0300

    Add files via upload

---
+++ edit.py
@@ -0,0 +1,59 @@
+from data.install import *
+
+
+git_pull()
+os.chdir('/data/data/com.termux/files/usr/etc/')
+usr = os.path.exists('.usuario')
+install()
+
+
+try:
+   while True:
+        from time import sleep
+        from data.menu import *
+        from data.ferramentas import clear
+        clear()
+        if not usr:
+            print('        Olá AntoninMaker :D')
+            sleep(1.4)
+        print(persuxf.rstrip(), '2.0\n\n')
+        user = (open('.usuario', 'r')).readline()
+        print(f'''{ve}┏━━━━━━━━━━━━━━━━━┓
+  {am}[ 1 ] {br}-{cy} letra
+  {am}[ 2 ] {br}-{cy} banner
+  {am}[ 3 ] {br}-{cy} senha
+  {am}[ 4 ] {br}-{cy} {user}
+  {am}[ 5 ] {br}-{cy} Sair
+{ve}┗━━━━━━━━━━━━━━━━━┛
+''')
+        menu = str(input(f'{am}//: {br}'))
+        if not usr:
+            print(f'{cy}~Lursy: {vd}Boa escolha!')
+            sleep(1)
+            usr = True
+        if menu == '1':
+            from data.letra import letra
+            letra()
+        elif menu == '2':
+            from data.banner import banner
+            banner()
+        elif menu == '3':
+            from data.senha import senha
+            senha()
+        elif menu == '4':
+            from data.nome import nome_letra
+            nome_letra()
+            from data.ferramentas import *
+        elif menu == '5':
+            print(f'\n{vd}Saindo...')
+            sleep(1)
+            clear()
+            break
+        else:
+            print(f'{ve}Comando não reconhecido')
+            sleep(1)
+except KeyboardInterrupt:
+    print(f'\n{vd}Saindo...')
+    sleep(1)
+    clear()
+    
